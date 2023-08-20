2a6c52e83f2f061d1786fc02ea6725f8e9a48b5f
Christian Martinez <christian@mixdlabs.com>
2023-08-20 17:25:30 -0600

    Setup controllers file. Currently empty, but we need the schema next to work on our get routes for thoughts and users. Importing it from the api, but of course nothing is there yet.

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -1,4 +1,8 @@
 const router = require('express').Router()
+const {
+  getThoughts,
+  createThought,
+} = require('../../controllers/thoughts-controller')
 /* /api/thoughts */
 router.route('/').get(getThoughts).post(createThought)
 
--- routes/api/users-routes.js
+++ routes/api/users-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { getUsers, createUser } = require('../../controllers/users-controller')
 /* /api/users */
 router.route('/').get(getUsers).post(createUser)
 
